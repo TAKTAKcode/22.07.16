5fa0bec16ca198c40aa0b7c60751878cb695cb6e
tak <wontaek22@gmail.com>
2022-07-16 16:59:33 +0900

    22.07.16
    Minumum

---
+++ src/com/tak/method/Sign.java
@@ -0,0 +1,31 @@
+package com.tak.method;
+
+import java.util.Scanner;
+
+public class Sign {
+
+    static int signOf(int n) {
+
+
+        int sign = 0;
+
+        if (n > 0) {
+            sign = 1;
+        } else if (n < 0) {
+            sign = -1;
+        }
+
+        return sign;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("정수 x : ");
+        int x = sc.nextInt();
+
+        int s = signOf(x);
+        System.out.println("sign(x)는 " + s + "입니다~");
+
+    }
+}
